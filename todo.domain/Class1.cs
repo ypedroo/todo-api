87d894607130df842a9f5e280131a7463ada32f6
ypedroo <ynoa.pedro@outlook.com>
2021-08-24 09:32:46 -0300

    add base entity

--- todo.domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace todo.domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ todo.domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace todo.domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Entity() 
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; }
+
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
