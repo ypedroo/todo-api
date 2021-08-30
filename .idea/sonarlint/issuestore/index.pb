6328ec35b52de2c0119c8bea16f4fa62f09a2ac8
ypedroo <ynoa.pedro@outlook.com>
2021-08-30 09:11:45 -0300

    add entity for todo

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+D
+todo.infra/Class1.cs,d\7\d75223f79bfd1c20d35967ab6497b51229a62e8c
---
+++ todo.domain/Entities/TodoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace todo.domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(string title, DateTime date, string refUser)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            RefUser = refUser;
+        }
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string RefUser { get; private set; }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+        
+        public void MarkAsUndone()
+        {
+            Done = false;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
