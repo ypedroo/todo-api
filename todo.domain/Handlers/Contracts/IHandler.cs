9a7b8b6f97af359c4bbeec0ee0954affd10a6ff9
ypedroo <ynoa.pedro@outlook.com>
2021-09-05 16:39:41 -0300

    (feature) add commad handlers

---
+++ todo.domain/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,9 @@
+﻿using todo.domain.Commands.Contracts;
+
+namespace todo.domain.Handlers.Contracts
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
