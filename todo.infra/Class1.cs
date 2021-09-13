9c6bbc5ff252d91d67f5f2ab3168ce86dc53e393
ypedroo <ynoa.pedro@outlook.com>
2021-09-13 08:35:24 -0300

    (feature) create data context

--- todo.infra/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace todo.infra
-{
-    public class Class1
-    {
-    }
-}
---
+++ todo.infra/Contexts/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo.domain.Entities;
+
+namespace todo.infra.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
